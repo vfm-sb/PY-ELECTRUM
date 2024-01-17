6eb1b2277a06d263751c0f187acc59c874a4e867
VFM | SB <vfm_sb@proton.me>
2024-01-17 23:22:45 +0200

    Currency >> Made it Hashable and Comparable
    - New: __hash__(), __eq__(), __ne__()
    + Custom Exception: ObjectMismatchError
    
    modified:   electrum/currency/currency.py
    new file:   electrum/exceptions/generic_exceptions.py

---
+++ electrum/exceptions/generic_exceptions.py
@@ -0,0 +1,16 @@
+"""Generic Exceptions for Py-Electrum"""
+
+
+class ObjectMismatchError(TypeError):
+    def __init__(
+        self,
+        message: str = "Objects Must Match",
+        expected: object | None = None,
+        received: object | None = None
+    ) -> None:
+        self.message = message
+        if expected and received:
+            expected_msg = f"Expected: {expected.__class__.__name__}"
+            received_msg = f"Received: {received.__class__.__name__}"
+            self.message += f" >> {expected_msg}, {received_msg}."
+        super().__init__(self.message)

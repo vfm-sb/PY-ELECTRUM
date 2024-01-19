13964c862e63b217a59dcebe3ba0d74a4c352458
VFM | SB <vfm_sb@proton.me>
2024-01-19 16:30:55 +0200

    Electrum Now Supports Top Level Imports
    > from electrum import Money
    
    modified:   .gitignore
    modified:   electrum/__init__.py

---
+++ electrum/__init__.py
@@ -0,0 +1,8 @@
+# Money Imports
+from .money.money import Money
+from .money.coin import Coin
+from .money.note import Note, Banknote
+from .money.cash import Cash
+
+# Other Imports
+from .currency.currency_builder import CurrencyBuilderCLI

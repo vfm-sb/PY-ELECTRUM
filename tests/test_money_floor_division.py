bc36d28752e4ad3bd00ba93e5723f0ec50c8e1b6
VFM | SB <vfm_sb@proton.me>
2024-01-28 12:15:27 +0200

    Electrum Tests >> TestMoneyFloorDivision & TestMoneyModuloDivision -- Core Tests
    new file:   tests/test_money_floor_division.py
    new file:   tests/test_money_modulo.py

---
+++ tests/test_money_floor_division.py
@@ -0,0 +1,114 @@
+"""
+Tests for Money Floor-Divisions
+"""
+# pylint: disable=expression-not-assigned
+
+# Built-in Modules
+from decimal import Decimal
+
+# Third-Party Modules
+import pytest
+
+# Local Modules
+from electrum import Money, Currency
+
+# Custom Exceptions
+from electrum.exceptions import CurrencyMismatchError
+
+
+class TestMoneyFloorDivision:
+
+    def test_floor_division_of_money_by_money_default(self):
+        result = Money(3, "EUR") // Money(2, "EUR")
+        assert result == 1
+        result = Money(4, "EUR") // Money(1.5, "EUR")
+        assert result == 2
+        result = Money(6.4, "EUR") // Money(2.2, "EUR")
+        assert result == 2
+        result = Money(3, "EUR") // Money("2", "EUR")
+        assert result == 1
+        result = Money("3", "EUR") // Money(2, "EUR")
+        assert result == 1
+        result = Money(4, "EUR") // Money("1.5", "EUR")
+        assert result == 2
+        result = Money("4", "EUR") // Money(1.5, "EUR")
+        assert result == 2
+        result = Money(6.4, "EUR") // Money("2.2", "EUR")
+        assert result == 2
+        result = Money("6.4", "EUR") // Money(2.2, "EUR")
+        assert result == 2
+        result = Money("3", "EUR") // Money("2", "EUR")
+        assert result == 1
+        result = Money("4", "EUR") // Money("1.5", "EUR")
+        assert result == 2
+        result = Money("6.4", "EUR") // Money("2.2", "EUR")
+        assert result == 2
+        result = Money(3, "EUR") // Money(Decimal("2"), "EUR")
+        assert result == 1
+        result = Money(Decimal("3"), "EUR") // Money(2, "EUR")
+        assert result == 1
+        result = Money(Decimal("3"), "EUR") // Money(Decimal("2"), "EUR")
+        assert result == 1
+
+    def test_floor_division_of_money_by_numeric_default(self):
+        money = Money(3, "EUR") // 2
+        assert money.amount == 1.0
+        assert money.currency == Currency("EUR")
+        money = Money(4, "EUR") // 1.5
+        assert money.amount == 2.0
+        money = Money(6.4, "EUR") // 2.2
+        assert money.amount == 2.0
+        money = Money(3, "EUR") // "2"
+        assert money.amount == 1.0
+        money = Money("3", "EUR") // 2
+        assert money.amount == 1.0
+        money = Money(4, "EUR") // "1.5"
+        assert money.amount == 2.0
+        money = Money("4", "EUR") // 1.5
+        assert money.amount == 2.0
+        money = Money(6.4, "EUR") // "2.2"
+        assert money.amount == 2.0
+        money = Money("6.4", "EUR") // 2.2
+        assert money.amount == 2.0
+        money = Money("3", "EUR") // "2"
+        assert money.amount == 1.0
+        money = Money("4", "EUR") // "1.5"
+        assert money.amount == 2.0
+        money = Money("6.4", "EUR") // "2.2"
+        assert money.amount == 2.0
+        money = Money(3, "EUR") // Decimal("2")
+        assert money.amount == 1.0
+        money = Money(Decimal("3"), "EUR") // 2
+        assert money.amount == 1.0
+        money = Money(Decimal("3"), "EUR") // Decimal("2")
+        assert money.amount == 1.0
+
+    def test_working_floor_divisions_money_by_money(self):
+        result = Money(20, "BGN") // Money(3.12, "BGN")
+        assert result == 6
+        result = Money(50, "EUR") // Money(4.99, "EUR")
+        assert result == 10
+        result = Money(225, "GBP") // Money(11.70, "GBP")
+        assert result == 19
+        result = Money(30_200, "TRY") // Money(1329.00, "TRY")
+        assert result == 22
+        result = Money(160_837, "JPY") // Money(1775, "JPY")
+        assert result == 90
+        result = Money(305_955.02, "USD") // Money(2400, "USD")
+        assert result == 127
+
+    def test_working_floor_divisions_money_by_numeric(self):
+        money = Money(20, "BGN") // 6
+        assert money.amount == 3.0
+        money = Money(50, "EUR") // 7
+        assert money.amount == 7.0
+        money = Money(225, "GBP") // 35
+        assert money.amount == 6.0
+        money = Money(30_200, "TRY") // 30
+        assert money.amount == 1006.0
+
+    def test_money_by_money_floor_divisions_with_currency_mismatch(self):
+        with pytest.raises(CurrencyMismatchError):
+            Money(2, "EUR") // Money(2, "BGN")
+        with pytest.raises(CurrencyMismatchError):
+            Money(2, "BGN") // Money(2, "EUR")

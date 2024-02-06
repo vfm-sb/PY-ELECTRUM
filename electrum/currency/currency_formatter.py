8c83716e1d63bfabdba4506d3a34eda45b38aec8
VFM | SB <vfm_sb@proton.me>
2024-02-06 04:11:49 +0200

    CurrencyFormatter >> Initial: name_format() & Parts
    + unit_name_format()
    + subunit_name_format()
    
    modified:   electrum/currency/currency_formatter.py

--- electrum/currency/currency_formatter.py
+++ electrum/currency/currency_formatter.py
@@ -18,3 +18,22 @@ class CurrencyFormatter:
 
     def financial_rtl(self) -> str:
         return f"{self.amount} {self.currency.alphabetic_code}"
+
+    def name_format(self) -> str:
+        if self.amount >= 1:
+            return self.unit_name_format()
+        return self.subunit_name_format()
+
+    def unit_name_format(self) -> str:
+        if self.amount > 1 and self.currency.unit_plural:
+            return f"{self.amount} {self.currency.unit_plural.capitalize()}"
+        return f"{self.amount} {self.currency.unit_name.capitalize()}"
+
+    def subunit_name_format(self) -> str:
+        base_amount = int(self.amount * self.currency.denominator)
+        if self.amount > self.currency.base and self.currency.subunit_plural:
+            return f"{base_amount} {self.currency.subunit_plural.capitalize()}"
+        if self.currency.subunit_name:
+            return f"{base_amount} {self.currency.subunit_name.capitalize()}"
+        return self.unit_name_format()
+
